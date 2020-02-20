f1fdf2dfdab89c15e51004b55d3cb57c0b5ede88
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-21 02:23:51 +0300

    feat: add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import LinksList from '../LinksList/LinksList'
+import logo from '/RSSchool/Culture-Portal/src/assets/icons/book.svg';
+import linksList from '/RSSchool/Culture-Portal/src/data/links';
+import './Header.scss'
 
 function Header() {
   return (
-    <div></div>
+    <header className='header-container'>
+      <img className='logo-img' src={logo} alt=""/>
+      <h1 className='logo-name'>Writers of Belarus</h1>
+      <LinksList list={ linksList }/>
+    </header>
   );
 }
 
---
+++ src/data/links.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    name: 'Home',
+    destination: '/',
+  },
+  {
+    id: 2,
+    name: 'Authors',
+    destination: '/authors',
+  },
+  {
+    id: 3,
+    name: 'Team',
+    destination: '/team',
+  },
+  {
+    id: 4,
+    name: 'Style guide',
+    destination: '/styleguide',
+  },
+  {
+    id: 5,
+    name: 'Worklog',
+    destination: '/worklog',
+  },
+];
