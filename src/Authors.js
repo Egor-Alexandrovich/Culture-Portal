400f44b959ac48f0b07d49f05bc6dc92ee873de3
Artem <tommerg09@gmail.com>
2020-02-21 07:32:04 +0300

    react router

---
+++ src/Authors.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class Authors extends React.Component {
+    render() {
+      return (
+        <div>
+            <h1>Писатели Беларуси</h1>
+        </div>
+      )
+    }
+}
+
+export default Authors;
