fd96dd76a98cbecf99cf9fdaee3fc00f36700d21
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-23 16:36:29 +0300

    feat: add AuthorDescription layout

--- src/components/AuthorDescription/AuthorDescription.jsx
+++ src/components/AuthorDescription/AuthorDescription.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 
 function AuthorDescription() {
+  const { id } = useParams();
+  const index = data.findIndex(post => post.id === id);
+  const { name, years, picture, timeline, works, gallery, video, activity } = data[index];
+  const timelineElem = timeline ? <Timeline timeline={timeline}/> : null;
+  const worksElem = works ? <WorksList works={works}/> : null;
+  const galleryElem = gallery ? <PhotoGallery gallery={gallery}/> : null;
+  const videoElem = video ? <YouTubeVideo video={video}/> : null;
+  const mapElem = activity ? <ActivityMap activity={activity}/>  : null;
   return (
-    <div></div>
+    <div>
+      <AuthorIdentity 
+        name = { name }
+        years = { years }
+        picture = { picture }
+      />
+      { timelineElem, worksElem, galleryElem,videoElem, mapElem }
+    </div>
   );
 }
 
