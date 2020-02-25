ad3676968b768d803cca2d3ab7f77a79b796d1ae
Egor <alexandrovich2572@gmail.com>
2020-02-25 10:56:23 +0300

    feat: add autors page

--- src/components/Authors/Authors.jsx
+++ src/components/Authors/Authors.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
+import data from '../../data/testAuthors.js';
+import AuthorIdentity from '../AuthorIdentity/AuthorIdentity';
 
-function Authors({t}) {
-  return (
-    <div>
-        <h2>{t('Writers of Belarus')}</h2>
-    </div>
-  );
+import { withRouter } from 'react-router-dom';
+
+class Authors extends Component {
+  renderAuthors(arr) {
+    return arr.map((item) => {
+      const { name, years, photo, id } = item;
+      return (
+        <AuthorIdentity 
+        key = {id}
+        name = { name }
+        years = { years }
+        photo = { photo }
+        link = {true}
+        id = { id }
+      />
+      );
+    });
+  }
+  render () {
+    if (!data) {
+      return (
+        <div>
+        <h2>Писатели Беларуси</h2>
+      </div>
+      )
+    }
+
+    const items = this.renderAuthors(data);
+    return (
+      <div>
+        <h2>Писатели Беларуси</h2>
+          {items}
+      </div>
+      
+    )
+  }
 }
 
-export default withTranslation()(Authors);
+
+export default withTranslation()(withRouter(Authors));
