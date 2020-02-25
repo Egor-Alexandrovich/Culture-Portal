78c7a96713bfc6e734b3fabc8a124253756b2aa3
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-25 23:58:35 +0300

    feat: add video component

--- src/components/YouTubeVideo/YouTubeVideo.jsx
+++ src/components/YouTubeVideo/YouTubeVideo.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ModalVideo from 'react-modal-video';
+import './YouTubeVideo.scss';
+import '../../../node_modules/react-modal-video/scss/modal-video.scss';
 
-function YouTubeVideo() {
-  return (
-    <div>
-    </div>
-  );
+class YouTubeVideo extends Component {
+  constructor({videoId}) {
+    super()
+    this.state = {
+      isOpen: false,
+      videoId: videoId
+    }
+    this.openModal = this.openModal.bind(this)
+  }
+ 
+  openModal() {
+    this.setState({isOpen: true})
+  }
+ 
+  render() {
+    return (
+      <div className="wrapper">
+        <h2>Видео об авторе:</h2>
+        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={this.state.videoId} onClose={() => this.setState({isOpen: false})} />
+        <button className="btn btn-dark" onClick={this.openModal}>Смотреть</button>
+      </div>
+    )
+  }
 }
 
 export default YouTubeVideo;
