ff36c64ae226c833e05dec68b185e70139c862b9
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-27 00:07:40 +0300

    feat: add map component

--- src/components/ActivityMap/ActivityMap.jsx
+++ src/components/ActivityMap/ActivityMap.jsx
@@ -1,10 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactMapGL, {Popup, NavigationControl, FullscreenControl, ScaleControl} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Pins from '../Pins/Pins';
+import './ActivityMap.scss';
 
-function ActivityMap() {
-  return (
-    <div>
-    </div>
-  );
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWpvbG9sciIsImEiOiJjazQ0Znp0eHEwZ3ZvM21taXRoYzMxcmo2In0.Ve_XTroM0feG7vGKBYigAA';
+
+class ActivityMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 53.9006,
+        longitude: 27.5590,
+        zoom: 10,
+        bearing: 0,
+        pitch: 0
+      },
+      popupInfo: null,
+      places: props.activity
+    };
+  }
+
+  _updateViewport = viewport => {
+    this.setState({viewport});
+  };
+
+  _onClickMarker = place => {
+    this.setState({popupInfo: place});
+  };
+
+  _renderPopup() {
+    const {popupInfo} = this.state;
+
+    return (
+      popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={popupInfo.longitude}
+          latitude={popupInfo.latitude}
+          closeOnClick={false}
+          onClose={() => this.setState({popupInfo: null})}
+        >
+          <div>
+            <h2>{popupInfo.title}</h2>
+            <p>{popupInfo.description}</p>
+          </div>
+        </Popup>
+      )
+    );
+  }
+
+  render() {
+    const {viewport} = this.state;
+
+    return (
+      <div className="map-container">
+        <ReactMapGL
+          {...viewport}
+          width="100%"
+          height="100%"
+          mapStyle="mapbox://styles/mapbox/outdoors-v11"
+          onViewportChange={this._updateViewport}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        >
+          <Pins data={this.state.places} onClick={this._onClickMarker} />
+
+          {this._renderPopup()}
+
+          <div className="fullscreenControlStyle">
+            <FullscreenControl />
+          </div>
+          <div className="navStyle">
+            <NavigationControl />
+          </div>
+          <div className="scaleControlStyle">
+            <ScaleControl />
+          </div>
+        </ReactMapGL>
+      </div>
+    );
+  }
 }
 
 export default ActivityMap;
