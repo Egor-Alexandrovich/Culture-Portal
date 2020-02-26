e0e955a7b73b500a8ca05af2ded81cb2e33a23bc
Ilya Androsov <ilya_androsov@icloud.com>
2020-02-26 21:07:51 +0300

    feat: add photo gallery component

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/scss/image-gallery.scss';
 
-function PhotoGallery() {
-  return (
-    <div>
-    </div>
-  );
+function PhotoGallery({gallery}) {
+  return <ImageGallery items={gallery} />;
 }
 
 export default PhotoGallery;
